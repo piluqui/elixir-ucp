e7aa6a129f750eaae837f48a8234cd6edc1c9d3b
piluqui <piluqui@gmail.com>
2017-10-04 16:48:52 -0300

    Primer Ejercicio en Elixir (defmodule)

---
+++ 01-acronimo/acronimo.exs
@@ -0,0 +1,12 @@
+defmodule UCP do
+def acronimo(x) do
+    #demo
+    
+    demo = Regex.replace(~r/[a-z]/,x,"")
+    demo = Regex.replace(~r/\s/,demo,"")
+    demo = Regex.replace(~r/[áéíóúÁÉÍÓÚ]/,demo,"")
+    IO.puts "demo= #{demo}"
+end
+end
+
+UCP.acronimo("Asociación del Futbol Argentino")
